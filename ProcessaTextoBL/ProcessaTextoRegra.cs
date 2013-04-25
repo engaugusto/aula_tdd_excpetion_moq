b7f19870b678f05f67592ac7e69a054559b4540e
Carlos Augusto <guto.pro@gmail.com>
2013-04-24 21:36:06 -0300

    Processa texto

---
+++ ProcessaTextoBL/ProcessaTextoRegra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProcessaTextoBL
+{
+    public class ProcessaTextoRegra
+    {
+        public List<int> ConverteEntradaEmDias(string entradaUsu)
+        {
+            List<int> retDias = new List<int>();
+            for (int i = 0; i < entradaUsu.Length; i++)
+            {
+                if (entradaUsu[i] == 'B')
+                    retDias.Add(i + 1);
+            }
+
+            return retDias;
+        }
+    }
+}
---
+++ ProcessoTextoTest/ProcessaTextoRegraTest.cs
@@ -0,0 +1,106 @@
+﻿using ProcessaTextoBL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessoTextoTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ProcessaTextoRegraTest and is intended
+    ///to contain all ProcessaTextoRegraTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProcessaTextoRegraTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for ConverteEntradaEmDias
+        ///</summary>
+        [TestMethod()]
+        public void ConverteEntradaEmDiasTest()
+        {
+            ProcessaTextoRegra target = new ProcessaTextoRegra(); // TODO: Initialize to an appropriate value
+
+            StringBuilder entradaSimuladaUsuario = new StringBuilder();
+            for (int i = 0; i < 30; i++)
+            {
+                entradaSimuladaUsuario.Append("X");
+            }
+            entradaSimuladaUsuario.Remove(0, 1);
+            entradaSimuladaUsuario.Insert(0, "B");
+            
+            List<int> expected = new List<int>();
+            expected.Add(1);
+
+            List<int> actual = target.ConverteEntradaEmDias(entradaSimuladaUsuario.ToString());
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void VerificaExcecaoLimiteTamanhoTest()
+        { 
+            ProcessaTextoRegra target = new ProcessaTextoRegra(); // TODO: Initialize to an appropriate value
+
+            StringBuilder entradaSimuladaUsuario = new StringBuilder();
+            for (int i = 0; i < 5; i++)
+            {
+                entradaSimuladaUsuario.Append("X");
+            }
+            target.ConverteEntradaEmDias(entradaSimuladaUsuario.ToString());
+        }
+    }
+}
